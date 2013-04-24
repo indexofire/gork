eebd76a74852dbfd1fe053f9bb553a7377b9b927
Mark Renton <indexofire@gmail.com>
2013-04-24 10:52:57 +0800

    update

--- src/gork/settings/server.py
+++ src/gork/settings/server.py
@@ -19,5 +19,13 @@ DATABASES = {
 
 INSTALLED_APPS += (
     'gunicorn',
+    'article',
+    'article.module.category',
 )
 SECRET_KEY = os.environ["SECRET_KEY"]
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'hzcdclabs@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_PWD"]
+EMAIL_USE_TLS = True
