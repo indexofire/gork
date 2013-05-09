cfd59b536e0382d753e6ca1e013f22ce8571d8ba
Mark Renton <indexofire@gmail.com>
2013-05-09 16:48:00 +0800

    dif

--- src/gork/application/sale/models.py
+++ src/gork/application/sale/models.py
@@ -5,12 +5,13 @@ from sale.settings import STATUS_CHOICES
 
 
 class ShoppingItem(models.Model):
-    publisher = models.ForeignKey(settings.AUTH_USER_MODEL)
-    item_name = models.CharField(max_length=255)
-    item_info = models.CharField(max_length=1024)
-    add_time = models.DateTimeField(auto_now=True)
-    edit_time = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL)
+    name = models.CharField(max_length=255)
+    info = models.CharField(max_length=1024)
+    create_time = models.DateTimeField(auto_now=True)
+    lastedit_time = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES)
+    list = models.ForeignKey('ShoppingList', related_name='shopping_list')
 
     def __unicode__(self):
         return self.id
@@ -19,3 +20,8 @@ class ShoppingItem(models.Model):
         return
 
 
+class ShoppingList(models.Model):
+    create_time = models.DateTimeField()
+    lastedit_time = models.DateTimeField()
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL)
+    name = models.CharField(max_length=255)
